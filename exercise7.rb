aa93723cba7a2a03e29818efaf3f175916a12218
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 11:03:32 -0400

    Fixed display to actually output

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,12 @@
 students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
 
-def student_display(hash_in)
-	hash_in do |k, v|
+def student_display(hash_to_be_used)
+	hash_to_be_used.each do |k, v|
 		puts "#{k}: #{v} students"
 	end
 end
 
+student_display(students)
+
 students[:cohort4] = 43
+
