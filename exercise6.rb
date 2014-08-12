52f930cedd3a4507f1bd5586af179d9f894cd0be
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 09:51:08 -0400

    Added a banana check

--- exercise6.rb
+++ exercise6.rb
@@ -5,3 +5,9 @@ grocery_list << "rice"
 grocery_list_display = grocery_list.each {|x| puts "* #{x}\n"}
 
 puts "Number of items to pick up: #{grocery_list.length}"
+
+if grocery_list.include?("bananas")
+	puts "You need to pickup bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
