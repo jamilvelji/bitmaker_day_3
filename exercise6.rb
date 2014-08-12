6ca5aee6ea54c62f5667f659e9b080fa0b586eaf
Jamil Velji <jamil@jamilvelji.com>
2014-08-12 09:49:44 -0400

    Added a number of items count to the list

--- exercise6.rb
+++ exercise6.rb
@@ -3,3 +3,5 @@ grocery_list = ["water", "soap", "bread", "chicken"]
 grocery_list << "rice"
 
 grocery_list_display = grocery_list.each {|x| puts "* #{x}\n"}
+
+puts "Number of items to pick up: #{grocery_list.length}"
